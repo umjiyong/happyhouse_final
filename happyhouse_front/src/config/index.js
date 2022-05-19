b2e29d16033de3e9ced684fbfb882289358a5ae3
kschoi <kyusch0330@gmail.com>
2022-05-19 19:17:55 +0900

    [FRONT] branch develop

--- happyhouse_front/src/config/index.js
+++ happyhouse_front/src/config/index.js
@@ -3,4 +3,4 @@ const APT_DEAL_URL =
   "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev";
 
 export { API_BASE_URL, APT_DEAL_URL };
-// test
+// test develop
