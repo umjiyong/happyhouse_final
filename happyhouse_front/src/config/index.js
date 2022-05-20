d5f80ad7e6b36bb84d615d4c3920b511836ec87c
kschoi <kyusch0330@gmail.com>
2022-05-20 13:22:32 +0900

    [FRONT] re Remove Project

--- happyhouse_front/src/config/index.js
+++
@@ -1,8 +0,0 @@
-const API_BASE_URL = "http://localhost:8080/vue";
-const APT_DEAL_URL =
-  "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev";
-
-// spring server
-const HAPPYHOUSE_SPRING_URL = "http://localhost:8080/happyhouse";
-
-export { HAPPYHOUSE_SPRING_URL, API_BASE_URL, APT_DEAL_URL };
